aee0f38234bbf1deb1a5ae93066a8f5a700c758d
Streif.Tobias <Streif.Tobias@SK-D43D7EB3B863.2631.skool.net>
2017-09-20 16:29:14 +0200

    Collatz 1. Berechnung

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,8 +2,14 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HELLOWORLD!");
-
+	int n=19; 	
+	
+   if(n%2==0)
+   {
+	n=n/2;
+	}else{
+		n=3*n+1;
 	}
-
+System.out.println(n);
+}
 }
